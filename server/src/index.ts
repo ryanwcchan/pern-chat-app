bf5139b508caf23edd12b67247ae783b3609bf7b
Ryan Chan <122183743+TwelveStacks@users.noreply.github.com>
2024-12-16 13:59:12 -0500

    Setup react in client and express typescript in server

---
+++ server/src/index.ts
@@ -0,0 +1,16 @@
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.route";
+import messageRoutes from "./routes/message.route";
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT || 3000;
+
+app.use("/api/auth", authRoutes)
+app.use("/api/messages", messageRoutes)
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
---
+++ server/src/routes/auth.route.ts
@@ -0,0 +1,17 @@
+import express from "express";
+
+const router = express.Router();
+
+router.get("/login", (req, res) => {
+    res.send("Logged in successfully")
+})
+
+router.get("/logout", (req, res) => {
+    res.send("Logged out successfully")
+})
+
+router.get("/signup", (req, res) => {
+    res.send("Sign up successfully")
+})
+
+export default router;
