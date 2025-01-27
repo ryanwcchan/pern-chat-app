a338f70b7d1b7e0af27526618118563dd7a3b50c
Ryan Chan <122183743+TwelveStacks@users.noreply.github.com>
2025-01-26 20:57:14 -0500

    Add cors configuration

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,11 +4,21 @@ import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route";
 import messageRoutes from "./routes/message.route";
 import { app, server } from "./socket/socket";
+import cors from "cors";
 
 dotenv.config();
 
 const port = process.env.PORT || 3000;
+const allowedOrigin = process.env.VERCEL_URL
+  ? `https://${process.env.VERCEL_URL}` // e.g., https://your-vercel-project.vercel.app
+  : "*";
 
+app.use(
+  cors({
+    origin: allowedOrigin, // Allow requests from any origin
+    credentials: true, // If your API needs cookies to be sent
+  })
+);
 app.use(cookieParser());
 app.use(express.json());
 
