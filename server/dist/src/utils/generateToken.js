17415389b3cbdf94010f95b3c642b26603c99466
Ryan Chan <122183743+TwelveStacks@users.noreply.github.com>
2025-01-31 19:50:17 -0500

    Rebuild jwt token generator for production

--- server/dist/src/utils/generateToken.js
+++ server/dist/src/utils/generateToken.js
@@ -6,13 +6,13 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
 const generateToken = (id, res) => {
     const token = jsonwebtoken_1.default.sign({ id }, process.env.JWT_SECRET, {
-        expiresIn: "15d"
+        expiresIn: "15d",
     });
     res.cookie("jwt", token, {
         maxAge: 15 * 24 * 60 * 60 * 1000,
         httpOnly: true,
-        sameSite: "strict",
-        secure: process.env.NODE_ENV !== "development"
+        sameSite: "none",
+        secure: process.env.NODE_ENV !== "development",
     });
 };
 exports.default = generateToken;
