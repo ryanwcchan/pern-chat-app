b594c9244e15e201d38c4239e2971dcaf07b73e4
Ryan Chan <122183743+TwelveStacks@users.noreply.github.com>
2025-01-31 19:36:34 -0500

    fix: Change cookie sameSmite from strict to none

--- server/src/utils/generateToken.ts
+++ server/src/utils/generateToken.ts
@@ -1,17 +1,17 @@
-import jwt from "jsonwebtoken"
-import { Response } from "express"
+import jwt from "jsonwebtoken";
+import { Response } from "express";
 
 const generateToken = (id: string, res: Response) => {
-    const token = jwt.sign({id}, process.env.JWT_SECRET!, {
-        expiresIn: "15d"
-    })
+  const token = jwt.sign({ id }, process.env.JWT_SECRET!, {
+    expiresIn: "15d",
+  });
 
-    res.cookie("jwt", token, {
-        maxAge: 15 * 24 * 60 * 60 * 1000,
-        httpOnly: true,
-        sameSite: "strict",
-        secure: process.env.NODE_ENV !== "development"
-    })
-}
+  res.cookie("jwt", token, {
+    maxAge: 15 * 24 * 60 * 60 * 1000,
+    httpOnly: true,
+    sameSite: "none",
+    secure: process.env.NODE_ENV !== "development",
+  });
+};
 
-export default generateToken
+export default generateToken;
